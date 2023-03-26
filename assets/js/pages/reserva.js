6b432496bf3741647d3d1717e79959932a8fbe97
Diego Barreiro <diego.barreiro.perez@gmail.com>
2023-03-26 16:58:36 +0200

    Continuar

--- assets/js/pages/reserva.js
+++ assets/js/pages/reserva.js
@@ -5,7 +5,9 @@ $(document).ready(function () {
     $("#btn_continuar").on('click', () => {
         window.obtenerValor()
             .then(v => {
-                console.log("S", v);
+                const SESSIONSTORAGE_DATOSVUELO_KEY = "PA_DATOSVUELO";
+                sessionStorage.setItem(SESSIONSTORAGE_DATOSVUELO_KEY, JSON.stringify(v));
+                window.location.href = "/datos.html";
             });
     });
 });
